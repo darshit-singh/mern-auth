68314e8434ee18ef07796f12c66c7e8250ca4ba1
Darshit Sarna <op.dare7@gmail.com>
2023-06-19 23:52:19 +0530

    redux initial setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,8 @@ import './index.css'
 import HomeScreen from './screens/HomeScreen.jsx'
 import LoginScreen from './screens/LoginScreen.jsx'
 import RegisterScreen from './screens/RegisterScreen.jsx'
+import store from './store.js'
+import { Provider } from 'react-redux'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -19,7 +21,10 @@ const router = createBrowserRouter(
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      {/* This is our app with the routes */}
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </Provider>
 )
